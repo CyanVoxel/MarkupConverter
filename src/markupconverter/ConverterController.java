2348543f7f813473c6e00b74b1f5466e0bf3ac62
Travis Abendshien <lvnvtravis@gmail.com>
2019-07-22 18:17:29 -0700

    Added basic HTML5 support

--- src/markupconverter/ConverterController.java
+++ src/markupconverter/ConverterController.java
@@ -12,7 +12,8 @@ public class ConverterController {
 			// there is no need to convert!
 			output = input;
 			break;
-		case "HTML":
+		case "HTML5":
+			output = convertHtml5ToCommon(input, forceUl, cullTags);
 			break;
 		case "BBCode":
 			output = convertBbcodeToCommon(input, forceUl, cullTags);
@@ -26,7 +27,9 @@ public class ConverterController {
 		case "Markdown":
 			// Currently there's no need to convert!
 			break;
-		case "HTML":
+		case "HTML5":
+			if (input != "HTML5")
+				output = convertCommonToHtml5(output, forceUl);
 			break;
 		case "BBCode":
 			if (input != "BBCode")
@@ -90,6 +93,65 @@ public class ConverterController {
 	}
 
 	/**
+	 * Converts HTML5 to the internal common internal markup style.
+	 *
+	 * @param input The input text to convert
+	 * @return A string formatted to the common internal markup style.
+	 *
+	 */
+	private String convertHtml5ToCommon(String input, boolean forceUl, boolean cullTags) {
+		String output = input;
+
+		// Bold -------------------------------------------
+		output = output.replace("<b>", "**");
+		output = output.replace("</b>", "**");
+		// output = output.replace("<strong>", "**");
+		// output = output.replace("</strong>", "**");
+		if (!forceUl) {
+			output = output.replace("<b>", "__");
+			output = output.replace("</b>", "__");
+			// output = output.replace("<strong>", "__");
+			// output = output.replace("</strong>", "__");
+		}
+
+		// Italic -----------------------------------------
+		output = output.replace("<i>", "*");
+		output = output.replace("</i>", "*");
+		// output = output.replace("<em>", "*");
+		// output = output.replace("</em>", "*");
+		if (!forceUl) {
+			output = output.replace("<i>", "_");
+			output = output.replace("</i>", "_");
+			// output = output.replace("<em>", "_");
+			// output = output.replace("</em>", "_");
+		}
+
+		// Underline --------------------------------------
+		if (forceUl) {
+			output = output.replace("<u>", "_");
+			output = output.replace("</u>", "_");
+		} else if (cullTags) {
+			output = output.replace("<u>", "");
+			output = output.replace("</u>", "");
+		}
+
+		// Strikethrough ----------------------------------
+		output = output.replace("<s>", "~");
+		output = output.replace("</s>", "~");
+
+		// Blockquote -------------------------------------
+		output = output.replace("<blockquote>", "```");
+		output = output.replace("</blockquote>", "```");
+
+		// Code -------------------------------------------
+		output = output.replace("<code>", "`");
+		output = output.replace("</code>", "`");
+
+		return output;
+
+	} // convertHtml5ToCommon()
+
+	/**
 	 * Converts the internal common internal markup style to BBCode.
 	 *
 	 * @param input The input text to convert
@@ -114,6 +176,8 @@ public class ConverterController {
 				}
 			}
 		} // if (forceUl)
+
+		// @TODO Process lists here, looking for a newline + asterisks + space
 
 		// Italic -----------------------------------------
 		while (output.indexOf("*") >= 0) {
@@ -166,6 +230,89 @@ public class ConverterController {
 		}
 
 		return output;
-	}
+
+	} // convertCommonToBbcode()
+
+	/**
+	 * Converts the internal common internal markup style to HTML5.
+	 *
+	 * @param input The input text to convert
+	 * @return A string formatted to BBCode
+	 *
+	 */
+	private String convertCommonToHtml5(String input, boolean forceUl) {
+		String output = input;
+
+		// Bold -------------------------------------------
+		while (output.indexOf("**") >= 0) {
+			output = output.replaceFirst("\\*\\*", "<b>");
+			if (output.indexOf("**") >= 0) {
+				output = output.replaceFirst("\\*\\*", "</b>");
+			}
+		}
+		if (!forceUl) {
+			while (output.indexOf("__") >= 0) {
+				output = output.replaceFirst("__", "<b>");
+				if (output.indexOf("__") >= 0) {
+					output = output.replaceFirst("__", "</b>");
+				}
+			}
+		} // if (forceUl)
+
+		// @TODO Process lists here, looking for a newline + asterisks + space
+
+		// Italic -----------------------------------------
+		while (output.indexOf("*") >= 0) {
+			output = output.replaceFirst("\\*", "<i>");
+			if (output.indexOf("*") >= 0) {
+				output = output.replaceFirst("\\*", "</i>");
+			}
+		}
+		if (!forceUl) {
+			while (output.indexOf("_") >= 0) {
+				output = output.replaceFirst("_", "<i>");
+				if (output.indexOf("_") >= 0) {
+					output = output.replaceFirst("_", "</i>");
+				}
+			}
+		} // if (forceUl)
+
+		// Underline --------------------------------------
+		if (forceUl) {
+			while (output.indexOf("_") >= 0) {
+				output = output.replaceFirst("_", "<u>");
+				if (output.indexOf("_") >= 0) {
+					output = output.replaceFirst("_", "</u>");
+				}
+			}
+		} // if (forceUl)
+
+		// Strikethrough ----------------------------------
+		while (output.indexOf("~") >= 0) {
+			output = output.replaceFirst("~", "<s>");
+			if (output.indexOf("~") >= 0) {
+				output = output.replaceFirst("~", "</s>");
+			}
+		}
+
+		// Blockquote -------------------------------------
+		while (output.indexOf("```") >= 0) {
+			output = output.replaceFirst("```", "<blockquote>");
+			if (output.indexOf("```") >= 0) {
+				output = output.replaceFirst("```", "</blockquote>");
+			}
+		}
+
+		// Code -------------------------------------------
+		while (output.indexOf("`") >= 0) {
+			output = output.replaceFirst("`", "<code>");
+			if (output.indexOf("`") >= 0) {
+				output = output.replaceFirst("`", "</code>");
+			}
+		}
+
+		return output;
+
+	} //convertCommonToHtml5()
 
 } // ConverterController class
